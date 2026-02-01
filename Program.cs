44125dc56d73e77c35de1ac7e9a6278fe6610035
Jamie Waugh <c4029539@newcastle.ac.uk.com>
2026-02-01 16:16:25 +0000

    Started to create GetRainScore method This method uses GetHoursAveragePrecipitation to get the average precipitation across the last 24, 72 and 168 hours. I am unsure about the logic and formula for getting the rain score so I think I want to update these or review them, they are keeping values very low and I'm not sure how accurate they are. However, regardless of formula it is hard to test as I am relying on UK weather to provide values that will stretch the logic. I need a better way to test.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using Microsoft.VisualBasic;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 using System.Net;
+using System.Runtime.InteropServices;
 /* 
 Logic for getting the historical precipitation
 */
@@ -18,18 +19,19 @@ class Weather
     static async Task Main(string[] args)
     {
         //Test GetWeatherValues
-        int score_test_1 = await GetWeatherValues("limestone", "NE1 7RU");
-        int score_test_2 = await GetWeatherValues("gritstone", "NOTAPOSTCODE*");
+        List<int> score_test_1 = await GetWeatherValues("limestone", "NE1 7RU");
+        List<int> score_test_2 = await GetWeatherValues("gritstone", "NOTAPOSTCODE*");
+        List<int> score_test_3 = await GetWeatherValues("sandstone", "EH1 2EN");
 
-        Debug.Assert(score_test_1 == 999, "Test 1 must be equal to 999, as postcode does exist");
-        Debug.Assert(score_test_2 == -1, "Test 2 must be equal to -1, as postcode does not exist");
-
+        Debug.Assert(score_test_1.Count() == 10, "Test 1 must be equal to 10, as postcode does exist and data is found so we expect 10 days of rain scores to be returned ");
+        Debug.Assert(score_test_2.Count() == 0, "Test 2 must be equal to 0, as postcode does not exist");
+        Debug.Assert(score_test_3.Count() == 10 , "Test 3 must be equal to 10, as postcode does exist and data is found so we expect 10 days of rain scores to be returned "); //Need to update test to check highscores
+        
         Console.WriteLine("Tests passed");
     }
     //Return rain score
-    static async Task<int> GetWeatherValues(string rock_type, string postcode)
+    static async Task<List<int>> GetWeatherValues(string rock_type, string postcode)
     {
-        int rain_score = -1;
 
         double[] location = await GetLatLon(postcode);
 
@@ -38,13 +40,16 @@ class Weather
         {
             Console.WriteLine("Error with postcode");
             //TO DO: Ask for post code again
-            return rain_score;
+            return [];
         }
 
         //Calculate rain score
         else
         {
             DateTime now = DateTime.Now;
+            string current_hour_string = now.ToString("HH");
+            //Should be safe as current_hour string will always only ever be castable
+            int current_hour = int.Parse(current_hour_string);
             //Get a week from today, we know that the present day is index 167 in the json
             DateTime week_ago = now.AddDays(-7);
             //Get 10 days into the future (for a forecast)
@@ -57,7 +62,6 @@ class Weather
             Console.WriteLine(url);
             //Create HttpClient
             HttpClient client = new HttpClient();
-
             try
             {
                 //Get response
@@ -80,13 +84,16 @@ class Weather
 
                 Console.WriteLine(hourly_precipitation.Length);
 
+                //TO DO: implement rain score here
+
+                return GetRainScore(current_hour, hourly_precipitation);
+
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Error with exception {e}");
+                return [];
             }
-            
-            return 999;
         }
 
         //TO DO: Now we have lat and lon, get weather data.
@@ -134,5 +141,56 @@ class Weather
         }
 
 
+    }
+
+    static List<int> GetRainScore(int current_hour, double[] hourly_precipitation)
+    {
+        //Rain score array
+        List<int> rain_score_list = new List<int>();
+        //167 is the index representing the start of present day, every 24 indexes is the start of the next day
+        for(int i=current_hour+167; i < hourly_precipitation.Length-24; i += 24)
+        {
+            int rain_score;
+            //Last 24 hours
+            double pr_24 = GetHoursAveragePrecipitation(i, 24, hourly_precipitation);
+            //Last 72 hours
+            double pr_72 = GetHoursAveragePrecipitation(i, 72, hourly_precipitation);
+            //Last 7 days
+            double pr_168 = GetHoursAveragePrecipitation(i, 168, hourly_precipitation);
+
+            //TO DO: Read from json for actual RPF and weights, for now we use placeholders
+            double w_24 = 2;
+            double w_72 = 1;
+            double w_168 = 0.5;
+            double RPF = 1;
+
+            rain_score = (int)Math.Ceiling((w_24 * pr_24 + w_72 * pr_72 + w_168 * pr_168)/RPF);
+
+            rain_score_list.Add(rain_score);
+        }
+
+        foreach(int score in rain_score_list)
+        {
+            Console.Write(score + ", ");
+        }
+        Console.WriteLine();
+        return rain_score_list;
+    }
+
+    static double GetHoursAveragePrecipitation(int current_hour_index, int hours, double[] hourly_precipitation)
+    {
+        double total_average_precipitation = 0.00;
+
+        //168: last 7 days, 72: last 3 days, 23: last day
+
+        //We want rainfall from + 6 hours into the future of present day to x hours from present hour in past
+        //Hourly Future rainfall not currently included
+
+        for (int i = current_hour_index-hours; i <= current_hour_index; i+=1)
+        {
+            total_average_precipitation += hourly_precipitation[i];
+        }
+
+        return total_average_precipitation/hours;
     }
 }
